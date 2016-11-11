6b8850972d95a579d492fdf9a5e42c44a23b6f11
emilykeenan <emilyjanekeenan@gmail.com>
2016-11-11 16:23:38 -0600

    buttons working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,3 +1,37 @@
+var numbers = {
+  x: '',
+  y: '',
+  operator: ''
+};
+
 $(document).ready(function() {
 console.log('dis is working');
+
+$('.number').on('click', function() {
+  var num = $(this).data('number');
+  storeXY(num);
+  appendDOM(numbers);
+  console.log(numbers);
 });
+
+
+$('.operator').on('click', function() {
+  numbers.operator = $(this).data('operator');
+console.log(numbers);
+});
+
+});
+
+function storeXY (num) {
+  if(numbers.operator === '') {
+    numbers.x += num;
+  } else {
+    numbers.y += num;
+  }
+}
+
+function appendDOM (numbers) = {
+  $('x').html(numbers.x);
+  $('operator').html(numbers.x);
+  $('y').html(numbers.y);
+}
